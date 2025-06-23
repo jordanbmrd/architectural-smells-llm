7e8a4a5f927e0d664c69b79e676d72d5783dedb5
Pol Guillou <polguillou2@gmail.com>
2025-06-23 15:04:08 +0100

    fix: remove erpnext from github

--- erpnext/v7.1.13/frappe-erpnext-4cd3868/erpnext/accounts/report/balance_sheet/balance_sheet.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-// License: GNU General Public License v3. See license.txt
-
-frappe.require("assets/erpnext/js/financial_statements.js", function() {
-	frappe.query_reports["Balance Sheet"] = erpnext.financial_statements;
-});
-
-
