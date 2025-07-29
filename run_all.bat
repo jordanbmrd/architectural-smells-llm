428b914e5bc185fc030ad1325e6405ad8b02d221
Jordan Baumard <P2937574@LEC-ADMIN.dmu.ac.uk>
2025-07-29 13:04:03 +0100

    feat: new files

---
+++ run_all.bat
@@ -0,0 +1,16 @@
+@echo off
+python AI\Script\clone_and_clean_releases.py https://github.com/esphome/aioesphomeapi
+python AI\Script\clone_and_clean_releases.py https://github.com/bramstroker/homeassistant-powercalc
+python AI\Script\clone_and_clean_releases.py https://github.com/bunkerity/bunkerweb
+python AI\Script\clone_and_clean_releases.py https://github.com/streamlit/streamlit
+python AI\Script\clone_and_clean_releases.py https://github.com/yt-dlp/yt-dlp
+python AI\Script\clone_and_clean_releases.py https://github.com/jumpserver/jumpserver
+python AI\Script\clone_and_clean_releases.py https://github.com/python-telegram-bot/python-telegram-bot
+python AI\Script\clone_and_clean_releases.py https://github.com/openai/openai-python
+python AI\Script\clone_and_clean_releases.py https://github.com/roboflow/supervision
+python AI\Script\clone_and_clean_releases.py https://github.com/celery/celery
+python AI\Script\clone_and_clean_releases.py https://github.com/locustio/locust
+python AI\Script\clone_and_clean_releases.py https://github.com/microsoft/qlib
+python AI\Script\clone_and_clean_releases.py https://github.com/huggingface/transformers
+python AI\Script\clone_and_clean_releases.py https://github.com/uvejota/homeassistant-edata
+pause
